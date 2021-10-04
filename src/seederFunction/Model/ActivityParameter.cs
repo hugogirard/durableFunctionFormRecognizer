a10112a87c1ab669fedc25150583a2d51f35872c
Hugo Girard <hugogirard28@hotmail.com>
2021-10-04 15:48:55 -0400

    push model

---
+++ src/seederFunction/Model/ActivityParameter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Seeder.Model
+{
+    public class ActivityParameter
+    {
+        public string Filename { get; set; }
+
+        public Stream Content { get; set; }
+    }
+}
