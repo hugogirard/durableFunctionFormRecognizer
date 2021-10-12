b4a6c78e6fd162b6450d584571c2b8dc470968de
hugogirard <hugo.girard@microsoft.com>
2021-10-12 08:59:23 -0400

    added gitignore and log script

--- scripts/setup_vm.ps1
+++ scripts/setup_vm.ps1
@@ -22,7 +22,7 @@
 #SOFTWARE.
 #------------------------------------------------------------------------------
 
-Start-Transcript
+Start-Transcript -Path C:\installation_log.txt -Append
 ## Install .NET Core 5.0
 # Invoke-WebRequest "https://dot.net/v1/dotnet-install.ps1" -OutFile "./dotnet-install.ps1" 
 # ./dotnet-install.ps1 -Channel 5.0 -InstallDir c:\dotnet
